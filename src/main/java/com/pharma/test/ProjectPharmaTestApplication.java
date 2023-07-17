92b5c08ccbd4f548766eae96a81dd39c98fb7ab8
Marcos M <marcos.morales@pharmagbc.com>
2023-07-17 13:00:49 -0500

    first commit

---
+++ src/main/java/com/pharma/test/ProjectPharmaTestApplication.java
@@ -0,0 +1,13 @@
+package com.pharma.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPharmaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPharmaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pharma/test/ProjectPharmaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pharma.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPharmaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
